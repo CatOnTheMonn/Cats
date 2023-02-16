fbcf9b9e0159a07e7b7b2377a4668d3d7e686f4b
Viacheslav Sukhenko <slavichmy@gmail.com>
2023-02-16 21:11:22 +0200

    edit code

--- a.py
+++ a.py
@@ -1,11 +1,14 @@
 from flask import Flask, session, render_template
 import random
+import os
 
 def root():
     session['count'] = 0
-    return'Hello world <br> <a href="/clicerOwO">≧◠ᴥ◠≦</a>'
+    # return'Hello world <br> <a href="/clicerOwO">≧◠ᴥ◠≦</a>'
+    return render_template('index.html')
 
-app = Flask(__name__)
+folder = os.getcwd()
+app = Flask(__name__, template_folder=folder, static_folder=folder)
 
 app.add_url_rule('/', 'root', root)
 app.config['SECRET_KEY'] = '123456789'
