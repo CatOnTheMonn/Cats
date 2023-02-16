b9a196f30116c4569b1a81bf59e168f477fb143c
CatOnTheMonn <xxcatxx14394@gmail.com>
2023-02-16 20:56:55 +0200

    ###########

--- a.py
+++ a.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, session, render_template
 import random
 
 def root():
-    return'Hello world' + str(random.randint(1, 100))
+    session['count'] = 0
+    return'Hello world <br> <a href="/clicerOwO">≧◠ᴥ◠≦</a>'
 
 app = Flask(__name__)
 
 app.add_url_rule('/', 'root', root)
+app.config['SECRET_KEY'] = '123456789'
 
 app.run()
